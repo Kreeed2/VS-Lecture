cfdbb319a26b1b891fda689b1ff432dc6b202822
Kreeed2 <Kreeed2@googlemail.com>
2017-10-22 14:37:36 +0200

    Finished Work; Added quorum

--- serverPolling.js
+++ serverPolling.js
@@ -1,54 +1,64 @@
-function queryServer() {
+var allEntries = [];
+var readQuorum = 3;
+var checkedServer = [];
+
+function queryServer(serverNumber) {
   var URL = "http://ipc772.inf-bb.uni-jena.de/brett/server";
   var ENDING = ".php?method=GET_MESSAGES";
-  
-  for (var i = 1; i < 10; i++) {
-	$.get(URL.concat(i,ENDING), function(data, status) {
-		if (status === "success") {
-			var obj = JSON.parse(data);
-			entry = Object.values(obj);
-			for (var j = 0; j < entry.length; j++) {
-				writeEntry(entry[j]);
-			}
-		}
-	});
-  }
-}
 
-function putEntryInList(entry, entries) {
-  var found = false;
-  for (var i = 0; i < entries.length; i++) {
-    if (entries[i].id === entry.id) {
-      if (entries[i].version < entry.version) {
-        entries[i] = entry;
-      } else {
-        found = true;
+  $.get(URL.concat(serverNumber,ENDING), function(data) {
+    checkedServer.push(serverNumber);
+    var entryFound = false;
+
+    for (var i = 0; i < data.length; i++) {
+      for (var j = 0; j < allEntries.length; j++) {
+        if (allEntries[j].id === data[i].id) { //Passender Eintag in Speicher gefunden
+          var entryFound = true;
+          if (allEntries[j].id < data[i].id) { //Abfrageeintrag ist neuer als Eintag im Speicher
+            allEntries[j] = data[i];
+          } else { //Abfrageeintrag ist älter als Eintag im Speicher
+            break;
+          }
+        }
+      }
+      if (entryFound === false) { //Neuen Eintag in den Speicher aufnehmen
+        allEntries.push(data[i]);
       }
     }
-  }
-  if (found === false) {
-    entries.push(entry);
-  }
+    if (checkedServer.length < readQuorum) {
+      while (true) {
+        var nextServer = Math.floor((Math.random() * 10) + 1); // Keine doppelten Serverabfragen
+        if (!(nextServer in checkedServer)) {
+          queryServer(nextServer);
+          break;
+        }
+      }
+    } else {
+      for (var i = 0; i < allEntries.length; i++) {
+        writeEntry(allEntries[i]);
+      }
+    }
+  }, "json");
 }
 
 function writeEntry(jsonObj) {
   var li = document.createElement("li");
   li.setAttribute('identifier', jsonObj.id);
   li.setAttribute('version', jsonObj.version);
-  
+
   var h = document.createElement("h4");
   var p = document.createElement("p");
   var s = document.createElement("small");
- 
+
   h.innerHTML = jsonObj.author;
   p.innerHTML = jsonObj.msg;
   s.innerHTML = jsonObj.creation;
-  
+
   li.appendChild(h);
   li.appendChild(p);
   li.appendChild(s);
-  
+
   document.getElementById("entryList").appendChild(li);
 }
 
-$(document).ready(queryServer());
+$(document).ready(queryServer(1));
